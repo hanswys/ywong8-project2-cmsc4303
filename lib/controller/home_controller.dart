805d6078aa56aeed33062af3181a7fd6b34c5b79
Yong Shen Wong <ywong8@uco.edu>
2024-03-14 17:14:03 -0500

    laggy commit

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,8 @@
+import '../view/home_screen.dart';
+
+class HomeController {
+  HomeState state;
+  HomeController(this.state);
+
+  Future<void> signOut() async {}
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lesson6/controller/home_controller.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return HomeState();
@@ -8,6 +11,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeState extends State<HomeScreen> {
+  late HomeController con;
+
+  @override
+  void initState() {
+    super.initState();
+    con = HomeController(this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +26,24 @@ class HomeState extends State<HomeScreen> {
         title: const Text('Home'),
       ),
       body: const Text('Home'),
+      drawer: drawerView(context),
     );
+  }
+
+  Widget drawerView(BuildContext context) {
+    return Drawer(
+        child: ListView(
+      children: [
+        UserAccountsDrawerHeader(
+          accountName: const Text('No profile'),
+          accountEmail: const Text('email'),
+        ),
+        ListTile(
+          leading: const Icon(Icons.logout),
+          title: const Text('Sign Out'),
+          onTap: con.signOut,
+        )
+      ],
+    ));
   }
 }
